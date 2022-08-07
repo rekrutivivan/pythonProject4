5ede58b90a27f0e2d7045cfd3a6d37000373d070
rekrutivivan <rekrutivivan@gmail.com>
2022-08-08 02:01:58 +0300

    створено функція для надсилання, видалення, і пінення повідомлень

--- main.py
+++ main.py
@@ -1,50 +1,45 @@
 import dataclasses
-
+import pytz
 import telebot
 import os
 import requests
 import time
 import schedule
-
+from datetime import datetime
+from pytz import timezone
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-def create_text_simple_schedule_message():
-    test_message = "Наступна пара через 10хв\nПара:{name}\nВикладач:{f}".format(name="Математични",f="апрп")
-    print(test_message)
-create_text_simple_schedule_message()
 
-API_KEY = os.getenv('API_KEY')
+bot_token = '5512808396:AAGjLNbjHGowFuVIzMGZCIUH3_at7nEsDWY'
+bot_chatID = '562854820'
+bot = telebot.TeleBot(bot_token)
 
-bot = telebot.TeleBot('5512808396:AAGjLNbjHGowFuVIzMGZCIUH3_at7nEsDWY')
+def create_text_simple_schedule_message(subject, teacher):
+    text_message = "Наступна пара через 10хв\nПара:{subject_name}\nВикладач:{teacher_name}".format(subject_name=subject,teacher_name=teacher)
+    return text_message
 
+#time, not used, but in the future, maybe maybe...
+format = " %H:%M"
+now_utc = datetime.now(timezone('UTC'))
+# print(now_utc.strftime(format))
+now_asia = now_utc.astimezone(timezone('Europe/Kiev'))
+# print(now_asia.strftime(format))
 
-@bot.message_handler(commands=['Greet'])
+@bot.message_handler(commands=['next'])
 def greet(message):
-    bot.reply_to(message, "hey")
-
-
-
-@bot.message_handler(commands=['hello'])
-def greet(message):
-    replymarkup = InlineKeyboardMarkup([[InlineKeyboardButton(text="Посилання", url="www.google.com")]])
-    bot.send_message(message.chat.id, text=test_message, reply_markup=replymarkup)
-
-
+    list_message_id = []
+    # потрібна ще одна функція, яка буде готувати пакети даних(ім'я викладча, назва пари, посилання і інше),скоріш за все тут будемо юзати словник
+    for i in range(0, 2):
+        replymarkup = InlineKeyboardMarkup([[InlineKeyboardButton(text="Посилання", url="www.google.com")]])
+        message = bot.send_message(message.chat.id, text=create_text_simple_schedule_message('Hello', "dsfs"),reply_markup=replymarkup)
+        list_message_id.append(message.message_id)
+        bot.pin_chat_message(bot_chatID, message.message_id, disable_notification=True)
+    time.sleep(5)
+    for id in list_message_id:
+        bot.unpin_chat_message(bot_chatID, id)
+        bot.delete_message(bot_chatID, id)
 bot.polling()
-@dataclasses
-class rozklad:
-    day:str
-    starttime:float
-    name: str
 
-# def telegram_bot_sendtext(bot_message):
-#     bot_token = '5512808396:AAGjLNbjHGowFuVIzMGZCIUH3_at7nEsDWY'
-#     bot_chatID = '562854820'
-#     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-#     response = requests.get(send_text)
-#
-#     return response.json()
-#
-#
+# не потрібний код, але най буде...
 # def telegram_bot_sendtext(bot_message):
 #     bot_token = '5512808396:AAGjLNbjHGowFuVIzMGZCIUH3_at7nEsDWY'
 #     bot_chatID = '562854820'
@@ -54,7 +49,7 @@ class rozklad:
 #
 #     return response.json()
 #
-#
+
 # def report():
 #     my_balance = 10  ## Replace this number with an API call to fetch your account balance
 #     my_message = "Current balance is: {}".format(my_balance)  ## Customize your message
